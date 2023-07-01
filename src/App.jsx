a11cd842418d33bb5cddaf63d8830b6113664aad
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-07-01 20:11:10 +0300

    error basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import {
   Navigate,
   Outlet,
-  RouterProvider,
-  createBrowserRouter,
+  Route,
+  // RouterProvider,
+  Routes,
+  // createBrowserRouter,
 } from "react-router-dom";
 import LeftBar from "./components/LeftBar/LeftBar";
 import Header from "./components/Navbar/Header";
 import HomePage from "./pages/HomePage/HomePage";
-import FriendsPage from "./pages/FriendsPage/FriendsPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import TweetsPage from "./pages/TweetsPage/TweetsPage";
+// import FriendsPage from "./pages/FriendsPage/FriendsPage";
+// import ProfilePage from "./pages/ProfilePage/ProfilePage";
+// import TweetsPage from "./pages/TweetsPage/TweetsPage";
 
 import css from "./app.module.css";
-import OthersPage from "./pages/OthersPage/OthersPage";
+// import OthersPage from "./pages/OthersPage/OthersPage";
 
 const App = () => {
   const Layout = () => {
@@ -32,48 +34,56 @@ const App = () => {
     );
   };
 
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Layout />,
-        children: [
-          {
-            path: "/",
-            element: <HomePage />,
-          },
-          {
-            path: "/tweets",
-            element: <TweetsPage />,
-          },
-          {
-            path: "/tweets/:id",
-            element: <ProfilePage />,
-          },
-          {
-            path: "/profile",
-            element: <ProfilePage />,
-          },
-          {
-            path: "/friends",
-            element: <FriendsPage />,
-          },
-          {
-            path: "/others",
-            element: <OthersPage />,
-          },
+  // const router = createBrowserRouter(
+  //   [
+  //     {
+  //       path: "/",
+  //       element: <Layout />,
+  //       children: [
+  //         {
+  //           path: "/",
+  //           element: <HomePage />,
+  //         },
+  //         {
+  //           path: "/tweets",
+  //           element: <TweetsPage />,
+  //         },
+  //         {
+  //           path: "/tweets/:id",
+  //           element: <ProfilePage />,
+  //         },
+  //         {
+  //           path: "/profile",
+  //           element: <ProfilePage />,
+  //         },
+  //         {
+  //           path: "/friends",
+  //           element: <FriendsPage />,
+  //         },
+  //         {
+  //           path: "/others",
+  //           element: <OthersPage />,
+  //         },
 
-          {
-            path: "*",
-            element: <Navigate to="/" />,
-          },
-        ],
-      },
-    ],
-    { basename: "/social_tweets" }
+  //         {
+  //           path: "*",
+  //           element: <Navigate to="/" />,
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   { basename: "/social_tweets" }
+  // );
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/" />} />
+        <Route path="/" element={<HomePage />} />
+      </Route>
+    </Routes>
   );
-
-  return <RouterProvider router={router}></RouterProvider>;
+  // <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
