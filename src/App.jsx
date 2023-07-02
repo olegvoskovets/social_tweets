a2e441a55629d71803d8cf089fce69a85dd3ac56
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-07-02 08:01:41 +0300

    set base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import {
 import LeftBar from "./components/LeftBar/LeftBar";
 import Header from "./components/Navbar/Header";
 import HomePage from "./pages/HomePage/HomePage";
-// import FriendsPage from "./pages/FriendsPage/FriendsPage";
-// import ProfilePage from "./pages/ProfilePage/ProfilePage";
-// import TweetsPage from "./pages/TweetsPage/TweetsPage";
+import FriendsPage from "./pages/FriendsPage/FriendsPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import TweetsPage from "./pages/TweetsPage/TweetsPage";
 
 import css from "./app.module.css";
 // import OthersPage from "./pages/OthersPage/OthersPage";
@@ -78,8 +78,12 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/" />} />
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
+        <Route path="/tweets" element={<TweetsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/profile:id" element={<ProfilePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
